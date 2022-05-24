532263ff60b5a3dff5226fc07a8ce5218dc394eb
mcgidoug <mcgidoug@gmail.com>
2022-05-23 21:19:59 -0500

    updated routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const thoughtRoutes = require("./thoughtRoutes");
 const userRoutes = require("./userRoutes");
 
-router.use("/courses", thoughtRoutes);
-router.use("/students", userRoutes);
+router.use("/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
